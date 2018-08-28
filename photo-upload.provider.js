84c451cce04ef569ac24ad942dab9224996effd6
vstarush <vstarush@gmail.com>
2018-08-28 13:55:13 -0400

    Add files via upload

---
+++ photo-upload.provider.js
@@ -0,0 +1,23 @@
+(function() {
+
+  'use strict';
+
+  angular.module('appName')
+    .config(['$stateProvider', function ($stateProvider) {
+      $stateProvider
+        .state('photoUpload', {
+          url: '/photoUpload',
+          templateUrl: 'app/photoUpload/photoUpload.html',
+          controller: 'photoUploadController'
+        });
+      $stateProvider
+        .state('photoUploadRecruiter', {
+          url: '/photoUpload',
+          templateUrl: 'app/photoUpload/photoUpload.html',
+          controller: 'photoUploadController',
+          params:{
+            recruiterIcon:true
+          }
+        });
+    }]);
+})();
